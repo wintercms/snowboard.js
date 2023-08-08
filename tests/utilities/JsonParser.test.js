3888db02c8aec79b82b0f78c5b02265e9fcafe36
Ben Thomson <git@alfreido.com>
2023-08-08 09:50:36 +0800

    Rewrite JsonParser to use JSON5 plus our own rules

--- tests/utilities/JsonParser.test.js
+++ tests/utilities/JsonParser.test.js
@@ -13,10 +13,58 @@ describe('JsonParser utility', () => {
         expect(Snowboard.jsonParser().parse('undefined')).toBeUndefined();
     });
 
+    it('parses booleans', () => {
+        expect(Snowboard.jsonParser().parse('true')).toEqual(true);
+        expect(Snowboard.jsonParser().parse('false')).toEqual(false);
+    });
+
+    it('parses numbers', () => {
+        expect(Snowboard.jsonParser().parse('7')).toEqual(7);
+        expect(Snowboard.jsonParser().parse('5.82341')).toEqual(5.82341);
+        expect(Snowboard.jsonParser().parse('-3')).toEqual(-3);
+        expect(Snowboard.jsonParser().parse('+1.987')).toEqual(1.987);
+        expect(Snowboard.jsonParser().parse('.8675309')).toEqual(0.8675309);
+    });
+
     it('parses a simple string', () => {
         expect(Snowboard.jsonParser().parse('test')).toEqual('test');
         expect(Snowboard.jsonParser().parse('"test"')).toEqual('test');
         expect(Snowboard.jsonParser().parse('"null"')).toEqual('null');
+        expect(Snowboard.jsonParser().parse('\'1.23\'')).toEqual('1.23');
         expect(Snowboard.jsonParser().parse('\'test\'')).toEqual('test');
+    });
+
+    it('parses a string that looks like an object', () => {
+        expect(Snowboard.jsonParser().parse('first: "second"')).toEqual({
+            first: 'second',
+        });
+        expect(Snowboard.jsonParser().parse('firstName: "Ben", surname: \'Thomson\'')).toEqual({
+            firstName: 'Ben',
+            surname: 'Thomson',
+        });
+    });
+
+    it('parses an actual object', () => {
+        expect(Snowboard.jsonParser().parse('{ first: "second" }')).toEqual({
+            first: 'second',
+        });
+        expect(Snowboard.jsonParser().parse('{ \'firstName\': "Ben", "surname": \'Thomson\' }')).toEqual({
+            firstName: 'Ben',
+            surname: 'Thomson',
+        });
+    });
+
+    it('parses a string that looks like an array', () => {
+        expect(Snowboard.jsonParser().parse('\'winter\', \'is\', \'cool\'')).toEqual([
+            'winter',
+            'is',
+            'cool',
+        ]);
+        expect(Snowboard.jsonParser().parse('\'winter\', \'is\', \'number\', 1')).toEqual([
+            'winter',
+            'is',
+            'number',
+            1,
+        ]);
     });
 });
