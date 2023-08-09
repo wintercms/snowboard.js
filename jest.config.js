6768c08e55d97653cd3c53526dfdee4d42eba387
Ben Thomson <git@alfreido.com>
2023-08-09 10:20:51 +0800

    Limit code coverage reporters

--- jest.config.js
+++ jest.config.js
@@ -36,12 +36,11 @@ const config = {
     // coverageProvider: "babel",
 
     // A list of reporter names that Jest uses when writing coverage reports
-    // coverageReporters: [
-    //   "json",
-    //   "text",
-    //   "lcov",
-    //   "clover"
-    // ],
+    coverageReporters: [
+      "text",
+      "lcov",
+      "cobertura"
+    ],
 
     // An object that configures minimum threshold enforcement for coverage results
     // coverageThreshold: undefined,
