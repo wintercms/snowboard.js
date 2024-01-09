5897b7cfff7e6909de28c7453b299d9522355030
Ben Thomson <git@alfreido.com>
2024-01-09 16:13:12 +0800

    Add check for Snowboard on browser version of Reactivity package

--- packages/reactivity/src/browser.js
+++ packages/reactivity/src/browser.js
@@ -2,6 +2,10 @@ import ReactivePluginBase from './abstracts/ReactivePluginBase';
 import ReactiveSingleton from './abstracts/ReactiveSingleton';
 import ReactiveComponent from './abstracts/ReactiveComponent';
 
+if (!window.Snowboard) {
+    throw new Error('Snowboard must be loaded before any other packages.');
+}
+
 window.Snowboard.addAbstract('ReactiveSingleton', ReactiveSingleton);
 window.Snowboard.addAbstract('ReactivePluginBase', ReactivePluginBase);
 window.Snowboard.addAbstract('ReactiveComponent', ReactiveComponent);
